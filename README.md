9bb947411193869647057c925147c929a678831e
Ashley Brooks <ashley.brooks.a@gmail.com>
2018-03-23 12:04:04 -0700

    Adding script to GH

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run this script:
+
+1. Install Python requests library - This can also be done by running `pip install -r requirements.txt`
+2. Enter API key into the script
+3. Run `python worknotes.py <Enter number of hours since last shift>` (for example, `python worknotes.py 8`)
--- worknotes.py
+++ worknotes.py
@@ -1,33 +1,30 @@
-import requests
 import json
+import requests
+import sys
+
 from datetime import datetime, timedelta
 
-api_token = "WTS8E_CuvcyrnLHEy_JR"
 
-
-def get_since(hours):
-    """Given number of hours as an integer, return the UTC date and time."""
-
-    now = datetime.utcnow()
-    since = now - timedelta(hours=hours)
-
-    return since
+api_token = ""
 
 
 def do_get_request(endpoint, resource, payload={}):
     """Handle all GET requests."""
 
     url = "https://api.pagerduty.com/{0}".format(endpoint)
+    #url = "https://requestb.in/th5li5th"
 
     headers = {
         "Authorization": "Token token={0}".format(api_token),
-        "Content-type": "application/json",
         "Accept": "application/vnd.pagerduty+json;version=2"
     }
 
-    payload["limit"] = 100    
+    payload["limit"] = 100 
+    payload["time_zone"] = 'UTC'   
 
-    r = requests.get(url, headers=headers, data=json.dumps(payload))
+    r = requests.get(url, headers=headers, params=payload)
+
+    return r.json()
 
     if r.status_code == 200:
         r = r.json()
@@ -39,18 +36,16 @@ def do_get_request(endpoint, resource, payload={}):
                 payload["offset"] = 100
 
                 while r["more"]:
-                    r = requests.get(url, headers=headers, data=json.dumps(payload)).json()
+                    r = requests.get(url, headers=headers, params=payload).json()
 
                     for i in r[resource]:
                         output[resource].append(i)
 
                     payload["offset"] += 100
-                
                 r = output
-            
             return r
-
         except:
+            print(sys.exc_type, sys.exc_info)
             return r
 
     else:
@@ -58,10 +53,17 @@ def do_get_request(endpoint, resource, payload={}):
             .format(code=r.status_code, error=r.text))
 
 
-def get_incidents_since():
+def get_incidents_since(hours):
     """Given a UTC date and time, return all incidents triggered in a PD account since that time."""
 
-    incidents = do_get_request("incidents", "incidents", {"since": str(get_since(16))})
+    def get_since(hours):
+
+        now = datetime.utcnow()
+        since = now - timedelta(hours=hours)
+
+        return since
+
+    incidents = do_get_request("incidents", "incidents", {"since": str(get_since(hours))})
 
     incident_ids = []
 
@@ -76,7 +78,7 @@ def get_incident_log_entries(incident_id):
 
     log_entries = do_get_request("incidents/{0}/log_entries".format(incident_id), "log_entries")
 
-    print("Incident ID: {0} \n".format(incident_id))
+    print("--------------- Incident ID: {0} ---------------\n".format(incident_id))
 
     for ile in log_entries["log_entries"]:
         print(ile["created_at"])
@@ -86,10 +88,28 @@ def get_incident_log_entries(incident_id):
             print("Worknote: " + ile["channel"]["summary"] + "\n")
 
 
+def display_worknotes(hours):
+    """Prints out worknotes for given number of hours."""
+
+    recent_incidents = get_incidents_since(hours)
+
+    for incident_id in recent_incidents:
+        get_incident_log_entries(incident_id)
+
+
+def check_inputs():
+    """Check sys arguments before running rest of script."""
+
+    if len(sys.argv) != 2:
+        print("Usage: This script is intended to print the latest worknotes on an account given a number of hours as a single integer.")
+        print("Enter `worknotes.py <number of hours>` to run this program.")
+        sys.exit()
+
+
 if __name__ == "__main__":
-    # get_incident_log_entries("PGGHZ4B")
-    do_get_request("incidents", "incidents")
+    check_inputs()
 
+    hours = int(sys.argv[1])
 
-
+    display_worknotes(hours)
 
